65eb0e412ac7e83afb6c6a194be9a7a3933767b3
Eduardo <eduardobrumassio@hotmail.com>
2022-07-05 18:17:31 -0300

    feature: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 import '../src/styles/Global.css'
 import { Checkbox } from './components/Check'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>Todo List</h1>
+      <Header />
       <Checkbox />
     </div>
   )
