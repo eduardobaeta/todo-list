59bac3ef9b998a3b91f8863292bd54041147fc01
Eduardo <eduardobrumassio@hotmail.com>
2022-07-06 14:12:30 -0300

    feature: create function change status done in task component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,7 +1,12 @@
 import styles from './Checkbox.module.css'
 
-export function Checkbox() {
+type checkBoxProps = {
+  checked: boolean;
+  onChange: () => void;
+}
+
+export function Checkbox(props: checkBoxProps) {
   return (
-    <input className={styles.checkbox} type="checkbox" name="checkbox" id="checkbox" />
+    <input className={styles.checkbox} onChange={props.onChange} checked={props.checked} type="checkbox" />
   )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react'
 import { Checkbox } from './Checkbox'
+import { RiDeleteBin5Line } from 'react-icons/ri'
+
 import styles from './Task.module.css'
 
-export function Task() {
+type taskProps = {
+  isDone: boolean;
+  label: string;
+}
+
+
+export function Task(props: taskProps) {
+
+  const [isDone, setIsDone] = useState(true)
+
+  function handleSetIsDone() {
+    setIsDone(!isDone)
+    console.log('Change')
+  }
+
   return (
     <div className={styles.container}>
-      <Checkbox />
-      <p>
-        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-      </p>
-      
+      <Checkbox onChange={handleSetIsDone} checked={isDone} />
+      {isDone ? (
+        <p className={styles.labelDone}>
+          <s>
+            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+          </s>
+        </p>
+      ) :
+        (<p className={styles.label}>
+          Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+        </p>)
+      }
+      <RiDeleteBin5Line className={styles.deleteIcon} size={24} />
     </div>
   )
 }
