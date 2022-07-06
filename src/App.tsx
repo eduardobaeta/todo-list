1d02df6efdc0775b184ce8aeef7ba57da4e60a92
Eduardo <eduardobrumassio@hotmail.com>
2022-07-06 13:35:46 -0300

    feature: create form create task and void tasks content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,55 @@
 import { useState } from 'react'
 
-import '../src/styles/Global.css'
-import { Checkbox } from './components/Check'
 import { Header } from './components/Header'
 
+import styles from '../src/App.module.css'
+import '../src/styles/Global.css'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [tasks, setTasks] = useState([]);
+
+  const isTasksVoid = tasks.length === 0;
 
   return (
-    <div>
+    <div className={styles.container}>
       <Header />
-      <Checkbox />
+
+      <main className={styles.content}>
+        <form className={styles.createTaskBar}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+          <button>
+            Criar
+          </button>
+        </form>
+
+        <div className={styles.tasksContainer}>
+          <header className={styles.headerTasksContainer}>
+            <div className={styles.createdTasks}>
+              <strong>Tarefas criadas</strong>
+              <span>0</span>
+            </div>
+
+            <div className={styles.doneTasks}>
+              <strong>Concluídas</strong>
+              <span>0</span>
+            </div>
+          </header>
+
+          {isTasksVoid ? (<div className={styles.voidTasks}>
+            <img src="./src/assets/clipboard.png" alt="" />
+            <p>
+              <b>Você ainda não tem tarefas cadastradas</b>
+              <br />
+              Crie tarefas e organize seus itens a fazer
+            </p>
+          </div>)
+            :
+            ''
+          }
+
+        </div>
+      </main>
     </div>
   )
 }
